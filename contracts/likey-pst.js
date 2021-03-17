8787786036d59f96b969d4dbc38154acc5401029
Ayaka Neko <neko@ayaka.moe>
2021-03-17 18:00:35 +0800

    :sparkles: Implemented Ticker for Likey PST contract
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- contracts/likey-pst.js
+++ contracts/likey-pst.js
@@ -1,3 +1,14 @@
+/**
+ * Likey PST Contract
+ * Version: 1.0.0
+ * 
+ * Copyright ©️ Arcucy.io
+ * 
+ * Author: Arcucy Team <i@arcucy.io>
+ * Assosiated With: Project LIKEY
+ * Source: https://github.com/AyakaLab/Growth-Contract
+ */
+
 class Ownable {
     /**
      * isOwner checks whether the address is the owner of this contract
@@ -16,7 +27,6 @@ class Ownable {
      * @param {*} target
      */
     static transferOwnership(state, caller, target) {
-        console.log(state, caller, target)
         if (!Ownable.isOwner(state.owner, caller)) {
             throw new ContractError('transferOwnership#: Caller is not the owner of this contract')
         }
@@ -104,6 +114,9 @@ class Utils {
         if (typeof(address) !== 'string') {
             throw new ContractError('isAddress#: Address is not string')
         }
+        if (address === 'undefined') {
+            throw new ContractError('isAddress#: Address is invalid')
+        }
         return /^([a-zA-Z0-9]|_|-)+$/.test(address)
     }
 
@@ -130,38 +143,109 @@ class Utils {
         const bKeys = Object.keys(b).sort();
         return JSON.stringify(aKeys) === JSON.stringify(bKeys);
     }
+
+    static ratioConversion(state) {
+        if (!/1:\d+\.\d+(?!\d+)/.test(state.ratio)) {
+            throw new ContractError('sponsorAdded#: Ratio is invalid')
+        }
+        let original = 1
+        let conversion = parseFloat(state.ratio.split(':').pop())
+        let iteration = 0
+
+        while (true) {
+            if (!Number.isInteger(conversion)) {
+                conversion = conversion * 10
+                iteration++
+                continue
+            }
+            break
+        }
+
+        for (let i = 0; i < iteration; i++) {
+            original = BigInt(original) * BigInt('10')
+        }
+        conversion = BigInt(conversion)
+        return { original, conversion }
+    }
 }
 
 class Ticker {
-    static _mint(state, recipient, data) {
+    static _updateHolders(state) {
+        const balancesState = JSON.parse(JSON.stringify(state.balances))
+        state.holders = String(Object.keys(balancesState).length)
+    }
+
+    static _updateTotalSupply(state) {
+        let init = BigInt('0')
+        const balancesState = JSON.parse(JSON.stringify(state.balances))
+        for (const i of Object.values(balancesState)) {
+            init = init + BigInt(i)
+        }
+
+        state.totalSupply = init.toString()
+    }
+
+    static _mint(state, recipient, quantity) {
         const balancesState = JSON.parse(JSON.stringify(state.balances))
         if (!balancesState.hasOwnProperty(recipient)) {
             Object.defineProperty(balancesState, recipient, {
-                value: "0",
+                value: '0',
                 writable: true,
                 enumerable: true
             })
         }
 
-        const finalValue = BigInt(balancesState[recipient]) + BigInt(data.quantity)
-        balancesState[recipient] = finalValue
-        state.balances[recipient] = balancesState
+        try {
+            BigInt(String(quantity))
+        } catch {
+            throw new ContractError('_mint#: Input quantity invalid, should be number string')
+        }
+
+        const finalValue = BigInt(balancesState[recipient]) + BigInt(quantity)
+        balancesState[recipient] = finalValue.toString()
+
+        state.balances = balancesState
     }
 
-    static _burn(state, target, data) {
+    static _burn(state, target, quantity) {
+        const balancesState = JSON.parse(JSON.stringify(state.balances))
         if (!balancesState.hasOwnProperty(target)) {
-            throw new ContractError('burn#: Target has no balance in this ticker or invalid')
+            throw new ContractError('_burn#: Target has no balance in this ticker or invalid')
         }
-        // const finalValue = SmartWeave.arweave.ar.sub(String(tickerState.balances[target]), String(data.quantity))
-        if (arweave.ar.isGreaterThan(String(data.quantity), tickerState.balances[target])) {
-            balancesState[target] = "0"
-            state.balances[target] = tickerState
+
+        try {
+            BigInt(String(quantity))
+        } catch {
+            throw new ContractError('_burn#: Input quantity invalid, should be number string')
+        }
+
+        if (BigInt(String(quantity) > BigInt(balancesState[target])) || BigInt(String(quantity)) === BigInt(balancesState[target])) {
+            delete balancesState[target]
+            state.holders = String(Object.keys(balancesState).length)
+            state.balances = balancesState
         }
         else {
-            const finalValue = BigInt(balancesState[target]) + BigInt(data.quantity)
-            balancesState[target] = finalValue
-            state.balances[target] = balancesState
+            const finalValue = BigInt(balancesState[target]) - BigInt(quantity)
+            balancesState[target] = finalValue.toString()
+            state.balances = balancesState
         }
+    }
+
+    static _transfer(state, sender, recipient, qty) {
+        const balancesState = JSON.parse(JSON.stringify(state.balances))
+        if (!balancesState.hasOwnProperty(recipient)) {
+            this._mint(state, recipient, qty)
+            balancesState[recipient] = state.balances[recipient]
+        } 
+        else {
+            const add = BigInt(balancesState[recipient]) + BigInt(String(qty))
+            balancesState[recipient] = add.toString()
+        }
+
+        const sub = BigInt(balancesState[sender]) - BigInt(String(qty))
+        balancesState[sender] = sub.toString()
+
+        state.balances = balancesState
     }
 
     /**
@@ -171,15 +255,17 @@ class Ticker {
      * @param {*} recipient     - minted ticker recipient
      * @param {*} data          - mint data
      */
-    static mint(state, caller, recipient, data) {
-        if (!(data.owner === caller)) {
-            throw new ContractError('mintForCreator#: Caller is not the creator of its own')
+    static mint(state, caller, recipient, quantity) {
+        if (!Ownable.isOwner(state.owner, caller)) {
+            throw new ContractError('mint#: Caller is not the creator of its own')
         }
         if (!Utils.isAddress(String(recipient))) {
-            throw new ContractError(`mintForCreator#: Recipient is not a address`)
+            throw new ContractError(`mint#: Recipient is not a address`)
         }
 
-        this._mint(state, recipient, data)
+        this._mint(state, recipient, quantity)
+        this._updateHolders(state)
+        this._updateTotalSupply(state)
         return state
     }
 
@@ -191,14 +277,16 @@ class Ticker {
      * @param {*} quantity      - quantity
      */
     static burn(state, caller, target, quantity) {
-        if (!(quantity === caller)) {
-            throw new ContractError('burnForCreator#: Caller is not the creator of its own')
+        if (!Ownable.isOwner(state.owner, caller)) {
+            throw new ContractError('burn#: Caller is not the creator of its own')
         }
         if (!Utils.isAddress(String(target))) {
-            throw new ContractError(`burnForCreator#: Target is not a address`)
+            throw new ContractError(`burn#: Target is not a address`)
         }
 
         this._burn(state, target, quantity)
+        this._updateHolders(state)
+        this._updateTotalSupply(state)
         return state
     }
 
@@ -207,6 +295,72 @@ class Ticker {
      * @param {*} state         - contract state
      * @param {*} caller        - contract caller
      * @param {*} recipient     - minted ticker recipient
+     * @param {*} qty           - transfer quantity
+     */
+    static transfer(state, caller, recipient, qty) {
+        if (!Utils.isAddress(String(recipient))) {
+            throw new ContractError(`transfer#: Target is not a address`)
+        }
+        const balancesState = JSON.parse(JSON.stringify(state.balances))
+        if (!balancesState.hasOwnProperty(caller)) {
+            throw new ContractError('transfer#: Sender(caller) is invalid')
+        }
+        try {
+            BigInt(String(qty))
+        } catch (e) {
+            throw new ContractError('transfer#: Sender(caller) input qty invalid, should be number string')
+        }
+        if (balancesState[caller] === '0' || BigInt(balancesState[caller]) < BigInt(String(qty))) {
+            throw new ContractError('transfer#: Sender(caller) is insufficient fund')
+        }
+
+        this._transfer(state, caller, recipient, qty)
+        this._updateHolders(state)
+        this._updateTotalSupply(state)
+        return state
+    }
+
+    /**
+     * sponsorAdded adds a sponsor along with coresponding value of pst according to its transaction quantity
+     * @param {*} state         - contract state
+     */
+    static sponsorAdded(state) {
+        const ratio = Utils.ratioConversion(state)
+        const quantity = BigInt(SmartWeave.transaction.quantity) * ratio.original / ratio.conversion
+        
+        this._mint(state, SmartWeave.transaction.owner, quantity)
+        this._updateHolders(state)
+        this._updateTotalSupply(state)
+
+        return state
+    }
+
+    /**
+     * donationAdded adds a donation records into state.donation
+     * @param {*} state         - contract state
+     * @param {*} caller        - contract caller
+     * @param {*} data          - donation data
+     */
+    static donationAdded(state, caller, data) {
+        const donations = [...state.donations]
+
+        if (!Utils.isAddress(data.statusId)) {
+            throw new ContractError(`donationAdded#: Status ID is not a address`)
+        }
+
+        const obj = {
+            statusId: data.statusId,
+            quantity: SmartWeave.transaction.quantity,
+            sender: caller
+        }
+
+        donations.push(obj)
+        state.donations = donations
+
+        return state
+    }
+}
+
      * @param {*} data          - mint data
      */
     static transfer(state, caller, recipient, data) {
@@ -218,17 +372,51 @@ export function handle(state, action) {
     const input = action.input
     const caller = action.caller
 
+    // mint write_contract_function
+    /**
+     * @param {String} recipient address
+     * @param {Object} data mint data
+     */
     if (input.function === 'mint') {
-        const res = Ticker.mint(state, caller, input.recipient, input.data)
+        const res = Ticker.mint(state, caller, input.recipient, input.quantity)
         return { state: res }
     }
     
+    // burn write_contract_function
+    /**
+     * @param {String} target address
+     * @param {Object} data burn data
+     */
     if (input.function === 'burn') {
-        const res = Ticker.burn(state, caller, input.target, input.data)
+        const res = Ticker.burn(state, caller, input.target, input.quantity)
         return { state: res }
     }
     
+    // transfer write_contract_function
+    /**
+     * @param {String} target address
+     * @param {String} qty quantity
+     */
     if (input.function === 'transfer') {
+        const res = Ticker.transfer(state, caller, input.target, input.qty)
+        return { state: res }
+    }
+
+    // sponsorAdded write_contract_function
+    /**
+     * @param {Object} data sponsor data
+     */
+    if (input.function === 'sponsorAdded') {
+        const res = Ticker.sponsorAdded(state)
+        return { state: res }
+    }
+
+    // donationAdded write_contract_function
+    /**
+     * @param {Object} data donation data
+     */
+    if (input.function === 'donationAdded') {
+        const res = Ticker.donationAdded(state, caller, input.data)
         const res = Ticker.transfer(state, caller, input.recipient, input.data)
         return { state: res }
     }
