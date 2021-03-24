26cf3a1388108db4c5586b5dec495e5c11507c3d
Ayaka Neko <neko@ayaka.moe>
2021-03-24 18:25:13 +0800

    :sparkles: Updated Contracts
    Signed-off-by: Ayaka Neko <neko@ayaka.moe>

--- post.js
+++ post.js
@@ -12,10 +12,16 @@ const arweave = Arweave.init({
 })
 
 const key = JSON.parse(Buffer.from(fs.readFileSync(path.resolve('/Users/ayaka/Downloads/neko.json'))).toString())
+const state = Buffer.from(fs.readFileSync(path.resolve('./contracts/likey-initial.json'))).toString()
 
 async function main() {
-    const res = await SmartWeave.interactWriteDryRun(arweave, key, '0YHMLXXBGGSNJssVSFBkHcfmE0q9pDnk1aNglBeT-Ik', { function: 'tag' }, [{name: 'testing', value: 'data-ayaka 2021'}])
-    console.log(JSON.stringify(res.state))
+    // const res = await SmartWeave.interactWriteDryRun(arweave, key, '0YHMLXXBGGSNJssVSFBkHcfmE0q9pDnk1aNglBeT-Ik', { function: 'tag' }, [{name: 'testing', value: 'data-ayaka 2021'}])
+    // console.log(JSON.stringify(res.state))
+
+    const id = await SmartWeave.simulateCreateContractFromTx(arweave, key, 'fN-nTV-Q6HX9wDPNo89CKpbUhC6nDLWlnic7QzRA1g0', state)
+    const status = await arweave.transactions.getStatus(id)
+    console.log(id)
+    console.log(status)
 }
 
 main()
